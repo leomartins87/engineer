29cdca1c497934525d14cbe5e4bcb5dbb8489161
leomartins87 <126143009+leomartins87@users.noreply.github.com>
2023-02-27 03:12:41 -0300

    Update main.tf

--- src/ubuntu/main.tf
+++ src/ubuntu/main.tf
@@ -1,1 +1,65 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+
+#Use an ssh-key pair
+
+locals {
+  ssh_user         = "ubuntu"
+  key_name         = "ssh-key-pair1"
+  private_key_path = "/tmp/ssh-key-pair1.pem"
+}
+
+
+#Create a security group to allow port connections
+
+resource "aws_security_group" "ubuntu-sec-group" {
+  name = "ubuntu_access"
+  #VPC
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+# Create an EC2 instance
+
+resource "aws_instance" "ubuntu-vm" {
+#        ami     = "ami-0e0adf56e7c126fb7" #Ubuntu 21.04 AMI not found automatically
+#        ami     = "ami-0c880bbd328a9091d" #Ubuntu 21.04 AMI not found automatically
+#        ami                         = "ami-0735c191cf914754d" #Ubuntu 22.04
+         ami     = "ami-0c4aac39c51578be6" #Ubuntu 21.04 Oregon
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.ubuntu-sec-group.id]
+  key_name                    = local.key_name
+  tags = {
+    Name        = "u21.local"
+    Description = "Ubuntu 21.04 lab vm"
+  }
+}
